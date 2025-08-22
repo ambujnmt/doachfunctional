be8ef3a7eab89de6996a50d504b671ef5b143215
Ritesh singh <reetesh@nmttechnologies.in>
2025-08-22 17:25:29 +0530

    Side bar

---
+++ pages/customer/profile/update.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UpdateProfile from '../../../components/Customer/Profile/UpdateProfile'
+
+export default function update() {
+  return (
+    <div>
+      <UpdateProfile />
+    </div>
+  )
+}
